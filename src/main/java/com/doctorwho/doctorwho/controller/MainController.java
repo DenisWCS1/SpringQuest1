7fb8aef346b4b03ea911a968fe84f535d5d3a3cd
DenisWCS1 <denis.perrisseau.dev@gmail.com>
2023-04-14 16:01:40 +0200

    first commit

---
+++ src/main/java/com/doctorwho/doctorwho/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.doctorwho.doctorwho.controller;
+
+import java.text.BreakIterator;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/doctor/{id}")
+    @ResponseBody
+    public String index(@PathVariable String id) {
+        String response;
+        switch (id) {
+            case "1":
+                response = "William Hartnell";
+                break;
+            case "10":
+                response = "David Tennant";
+                break;
+            case "13":
+                response = "Jodie Whittaker";
+                break;
+            default:
+                response = "error no doctor with this id";
+                break;
+        }
+        return response;
+    }
+}
